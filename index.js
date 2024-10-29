b56956f52bf17ee0d6df3a26c2b169841bdf3473
sarah alenezi <saraalenzii52@gmail.com>
2024-10-29 19:42:44 +0300

    submition

--- index.js
+++ index.js
@@ -8,4 +8,25 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+function convertCmToM(cm) {
+  return cm / 100;
+}
+let meters = convertCmToM(height);
+const BMI=(weight/meters**2) ;
+
+
+console.log(`${BMI} cm is equal to ${meters} meters.`);
+
+alert(BMI);
+
+if (BMI < 18.5) {
+  alert("You are underweight.");
+} else if (BMI >= 18.5 && BMI < 25) {
+  alert( "You have a healthy weight.");
+} else if (BMI >= 25 && BMI < 30) {
+  alert("You are overweight.");
+} else  if( BMI >= 30) {
+  alert( "You are obese.");
+}
+
 }
